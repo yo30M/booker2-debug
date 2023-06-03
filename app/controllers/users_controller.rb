00b99c7aa7bede569081d3c4310e698d209c73a5
yo30M <129662479+yo30M@users.noreply.github.com>
2023-06-03 08:02:36 +0000

    [Finish]応用課題4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,8 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @books = @user.books
     @book = Book.new
+    @following_users = @user.following_user
+    @follower_users = @user.follower_user
   end
 
   def index
@@ -22,6 +24,16 @@ class UsersController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def follows
+    user = User.find(params[:id])
+    @users = user.following_user
+  end
+
+  def followers
+    user = User.find(params[:id])
+    @user = user.follower_user
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,22 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :following_user, through: :follower, source: :followed
+  has_many :follower_user, through: :followed, source: :follower
+
+  def follow(user_id)
+    follower.create(followed_id: user_id)
+  end
+
+  def unfollow(user_id)
+    follower.find_by(followed_id: user_id).destroy
+  end
+
+  def following?(user)
+    following_user.include?(user)
+  end
 
   def get_profile_image(width, height)
     unless profile_image.attached?
