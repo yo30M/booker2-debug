0471a25ebfbdebeaf1368b20f0da2b7832618f83
yo30M <129662479+yo30M@users.noreply.github.com>
2023-06-03 01:21:13 +0000

    [Finish]Return

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,8 +3,6 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
-    @user = @book.user
-    @newbook = Book.new
     @book_comment = BookComment.new
   end
 
