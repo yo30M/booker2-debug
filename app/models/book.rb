27d293beaf835351c40e3bee97eecc85f11b3c5d
yo30M <129662479+yo30M@users.noreply.github.com>
2023-06-04 09:10:07 +0000

    [Finish]応用課題5

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,6 +3,20 @@ class Book < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
 
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @book = Book.where("title LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @book = Book.where("title LIKE?", "#{word}%")
+    elsif search == "backward_match"
+      @book = Book.where("title LIKE?", "%#{word}")
+    elsif search == "partial_match"
+      @book = Book.where("title LIKE?", "%#{word}%")
+    else
+      @book = Book.all
+    end
+  end
+
   validates :title, presence: true
   validates :body, presence: true, length: { maximum: 200 }
 
