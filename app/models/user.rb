ca628cf3a0819362f0488999e2c82f12d0ef967d
yo30M <129662479+yo30M@users.noreply.github.com>
2023-06-08 02:35:10 +0000

    [Finish]応用課題7c

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,7 @@ class User < ApplicationRecord
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :followings, through: :relationships, source: :followed
   has_many :followers, through: :reverse_of_relationships, source: :follower
+  has_many :group_users, dependent: :destroy
 
   def follow(user)
     relationships.create(followed_id: user.id)
--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,6 @@ Rails.application.routes.draw do
   	get 'followers' => 'relationships#followers', as: 'followers'
   end
   get "search" => "searches#search"
+  resources :groups, except: [:destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
