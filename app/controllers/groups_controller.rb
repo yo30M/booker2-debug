b5cd3a39d56eaa0c5cb8d3d5ce6bf77589ccfaa1
yo30M <129662479+yo30M@users.noreply.github.com>
2023-06-08 05:18:16 +0000

    Return

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,13 +5,11 @@ class GroupsController < ApplicationController
   def index
     @book = Book.new
     @groups = Group.all
-    @user = User.find(current_user.id)
   end
 
   def show
     @book = Book.new
     @group = Group.find(params[:id])
-    @user = User.find(params[:id])
   end
 
   def new
