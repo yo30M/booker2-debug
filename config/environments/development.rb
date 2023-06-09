5d547b549258ddd0c78b0a5eee2d75cfc89e277c
yo30M <129662479+yo30M@users.noreply.github.com>
2023-06-09 08:02:40 +0000

    [Finish]応用課題9

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -76,4 +76,14 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
   config.hosts.clear
   config.active_job.queue_adapter = :inline
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            ENV['KEY'],
+    password:             ENV['SECRET_KEY'],
+    authentication:       'login',
+    enable_starttls_auto: true
+  }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
   get "search" => "searches#search"
   resources :groups, only: [:new, :create, :index, :show, :edit, :update] do
     resource :group_users, only: [:create, :destroy]
+    get "new/mail" => "groups#new_mail"
+    get "send/mail" => "groups#send_mail"
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
